3154bf0de6bfd2005b146fa29f4ae9b1e201a1de
Ming Chen <v.mingchen@gmail.com>
2025-01-08 19:56:52 -0500

    Add benchmark
    Tests shows that hll-64 is more accurate for both small and large.
    
    Test:
    ```
    go test -v hyperloglog64_test.go  hyperloglog64.go hyperloglog.go murmur.go
    === RUN   TestStableHash
    --- PASS: TestStableHash (0.00s)
    === RUN   TestHll64ComparisonSmall
        hyperloglog64_test.go:58: 32-bit error 81,132 is greater than 64-bit error 46,700
        hyperloglog64_test.go:60: 64-bit vs 32-bit:
            better: 6572
            same:   1521
            worse:  1907
    --- PASS: TestHll64ComparisonSmall (6.62s)
    === RUN   TestHll64ComparisonLarge
        hyperloglog64_test.go:98: 32-bit error 11,940,289,058 is greater than 64-bit error 440,418,287
        hyperloglog64_test.go:100: 64-bit vs 32-bit:
            better: 10000
            same:   0
            worse:  0
    --- PASS: TestHll64ComparisonLarge (5.99s)
    PASS
    ok      command-line-arguments  12.931s
    ```
    
    Benchmarks shows that hll-64 speed is about the same as hll-32:
    ```
     go test -bench=. hyperloglog64_test.go  hyperloglog64.go hyperloglog.go
     murmur.go
     goos: darwin
     goarch: arm64
     cpu: Apple M1 Pro
     BenchmarkHllAdd32_12-10         848587278                1.365 ns/op
     BenchmarkHllAdd64_12-10         934048399                1.306 ns/op
     BenchmarkHllAdd32_18-10         848574028                1.384 ns/op
     BenchmarkHllAdd64_18-10         935021826                1.291 ns/op
     BenchmarkHllCount32_12-10         231363              5175 ns/op
     BenchmarkHllCount64_12-10         230778              5208 ns/op
     BenchmarkHllCount32_18-10           3604            330861 ns/op
     BenchmarkHllCount64_18-10           3598            331184 ns/op
     ```

--- hyperloglog64_test.go
+++ hyperloglog64_test.go
@@ -3,8 +3,8 @@ package hyperloglog
 import (
 	"testing"
 
+	"github.com/OneOfOne/xxhash"
 	humanize "github.com/dustin/go-humanize"
-	"github.com/spaolacci/murmur3"
 )
 
 func int2bytes(i int) []byte {
@@ -18,21 +18,28 @@ func uabs(a, b uint64) uint64 {
 	return b - a
 }
 
+func TestStableHash(t *testing.T) {
+	h := xxhash.Checksum64([]byte("hello world"))
+	if h != 5020219685658847592 {
+		t.Errorf("Expected 5020219685658847592, got %d", h)
+	}
+}
+
 func TestHll64ComparisonSmall(t *testing.T) {
-	hll32, _ := New(1 << 12)
-	hll64, _ := New64(1 << 12)
+	hll32, _ := New(1 << 18)
+	hll64, _ := New64(1 << 18)
 
 	err32 := uint64(0)
 	err64 := uint64(0)
 	worse := uint64(0)
 	same := uint64(0)
 	better := uint64(0)
-	for i := 0; i < 1000000; i++ {
-		hll32.Add(murmur3.Sum32(int2bytes(i)))
+	for i := 0; i < 10000; i++ {
+		hll32.Add(xxhash.Checksum32(int2bytes(i)))
 		e32 := uabs(hll32.Count(), uint64(i+1))
 		err32 += e32
 
-		hll64.AddHash(murmur3.Sum64(int2bytes(i)))
+		hll64.AddHash(xxhash.Checksum64(int2bytes(i)))
 		e64 := uabs(hll64.Count(), uint64(i+1))
 		err64 += e64
 
@@ -58,8 +65,8 @@ func TestHll64ComparisonLarge(t *testing.T) {
 	hll64, _ := New64(1 << 18)
 
 	for i := 0; i < 100000000; i++ {
-		hll32.Add(murmur3.Sum32(int2bytes(i)))
-		hll64.AddHash(murmur3.Sum64(int2bytes(i)))
+		hll32.Add(xxhash.Checksum32(int2bytes(i)))
+		hll64.AddHash(xxhash.Checksum64(int2bytes(i)))
 	}
 
 	err32 := uint64(0)
@@ -68,11 +75,11 @@ func TestHll64ComparisonLarge(t *testing.T) {
 	same := uint64(0)
 	better := uint64(0)
 	for i := 100000000; i < 100010000; i++ {
-		hll32.Add(murmur3.Sum32(int2bytes(i)))
+		hll32.Add(xxhash.Checksum32(int2bytes(i)))
 		e32 := uabs(hll32.Count(), uint64(i+1))
 		err32 += e32
 
-		hll64.AddHash(murmur3.Sum64(int2bytes(i)))
+		hll64.AddHash(xxhash.Checksum64(int2bytes(i)))
 		e64 := uabs(hll64.Count(), uint64(i+1))
 		err64 += e64
 
@@ -91,4 +98,70 @@ func TestHll64ComparisonLarge(t *testing.T) {
 		t.Logf("32-bit error %s is greater than 64-bit error %s", humanize.Comma(int64(err32)), humanize.Comma(int64(err64)))
 	}
 	t.Logf("64-bit vs 32-bit:\nbetter:\t%d\nsame:\t%d\nworse:\t%d\n", better, same, worse)
+}
+
+func benchAdd32(b *testing.B, m uint) {
+	hll32, _ := New(1 << m)
+	for i := 0; i < b.N; i++ {
+		hll32.Add(uint32(i))
+	}
+}
+
+func benchAdd64(b *testing.B, m uint) {
+	hll64, _ := New64(1 << m)
+	for i := 0; i < b.N; i++ {
+		hll64.AddHash(uint64(i))
+	}
+}
+
+func BenchmarkHllAdd32_12(b *testing.B) {
+	benchAdd32(b, 12)
+}
+
+func BenchmarkHllAdd64_12(b *testing.B) {
+	benchAdd64(b, 12)
+}
+
+func BenchmarkHllAdd32_18(b *testing.B) {
+	benchAdd32(b, 18)
+}
+
+func BenchmarkHllAdd64_18(b *testing.B) {
+	benchAdd64(b, 18)
+}
+
+func benchCount32(b *testing.B, m uint) {
+	hll32, _ := New(1 << m)
+	for i := 0; i < 10000; i++ {
+		hll32.Add(uint32(i))
+	}
+	for i := 0; i < b.N; i++ {
+		hll32.Count()
+	}
+}
+
+func benchCount64(b *testing.B, m uint) {
+	hll64, _ := New64(1 << m)
+	for i := 0; i < 10000; i++ {
+		hll64.AddHash(uint64(i))
+	}
+	for i := 0; i < b.N; i++ {
+		hll64.Count()
+	}
+}
+
+func BenchmarkHllCount32_12(b *testing.B) {
+	benchCount32(b, 12)
+}
+
+func BenchmarkHllCount64_12(b *testing.B) {
+	benchCount64(b, 12)
+}
+
+func BenchmarkHllCount32_18(b *testing.B) {
+	benchCount32(b, 18)
+}
+
+func BenchmarkHllCount64_18(b *testing.B) {
+	benchCount64(b, 18)
 }
